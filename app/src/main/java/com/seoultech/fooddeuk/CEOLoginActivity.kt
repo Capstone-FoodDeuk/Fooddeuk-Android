ae292679daf1cc4b47194b347f40606036eef50d
Kimchohee <chchgml10@gmail.com>
2021-05-20 01:46:14 +0900

    :hammer: 사장님용 로그인 Activity로 전환 구현

---
+++ app/src/main/java/com/seoultech/fooddeuk/CEOLoginActivity.kt
@@ -0,0 +1,11 @@
+package com.seoultech.fooddeuk
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CEOLoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ceo_login)
+    }
+}
--- app/src/main/java/com/seoultech/fooddeuk/MainActivity.kt
+++ app/src/main/java/com/seoultech/fooddeuk/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.seoultech.fooddeuk
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.seoultech.fooddeuk.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        selectUserType()
+    }
+
+    private fun selectUserType() {
+        binding.tvUserTypeCeo.setOnClickListener {
+            val intent = Intent(this, CEOLoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
