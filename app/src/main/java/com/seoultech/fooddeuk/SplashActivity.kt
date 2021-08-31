90a5c85d449dce5b76cdeaab53ab1c2b7e7041b9
minha <2minha721@gmail.com>
2021-08-31 21:18:34 +0900

    리뷰 확인뷰(수정)

--- app/src/main/java/com/seoultech/fooddeuk/SplashActivity.kt
+++ app/src/main/java/com/seoultech/fooddeuk/SplashActivity.kt
@@ -29,32 +29,11 @@ class SplashActivity : AppCompatActivity() {
             showUserTypeSelectView()
         }, 1000)
 
-        getHashKey()
-
     }
 
     private fun showUserTypeSelectView() {
         val intent = Intent(this, UserTypeSelectActivity::class.java)
         startActivity(intent)
         finish()
-    }
-
-    private fun getHashKey() {
-        var packageInfo: PackageInfo? = null
-        try {
-            packageInfo = packageManager.getPackageInfo(packageName, PackageManager.GET_SIGNATURES)
-        } catch (e: PackageManager.NameNotFoundException) {
-            e.printStackTrace()
-        }
-        if (packageInfo == null) Log.e("KeyHash", "KeyHash:null")
-        for (signature in packageInfo!!.signatures) {
-            try {
-                val md: MessageDigest = MessageDigest.getInstance("SHA")
-                md.update(signature.toByteArray())
-                Log.d("KeyHash", Base64.encodeToString(md.digest(), Base64.DEFAULT))
-            } catch (e: NoSuchAlgorithmException) {
-                Log.e("KeyHash", "Unable to get MessageDigest. signature=$signature", e)
-            }
-        }
     }
 }
