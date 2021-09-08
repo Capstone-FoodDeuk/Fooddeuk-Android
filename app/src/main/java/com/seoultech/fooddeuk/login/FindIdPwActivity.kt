d30fea22f52d6a7fbbe31fe617e04b9f028c1897
choheeis <chchgml10@gmail.com>
2021-09-09 01:06:59 +0900

    아이디 찾기 프래그먼트 디자인 구현

--- app/src/main/java/com/seoultech/fooddeuk/login/FindIdPwActivity.kt
+++ app/src/main/java/com/seoultech/fooddeuk/login/FindIdPwActivity.kt
@@ -2,35 +2,46 @@ package com.seoultech.fooddeuk.login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
+import com.seoultech.fooddeuk.R
 import com.seoultech.fooddeuk.databinding.ActivityFindIdPwBinding
 
 class FindIdPwActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityFindIdPwBinding
+    private val findIdFragment = FindIdFragment()
+    private val findPwFragment = FindPwFragment()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityFindIdPwBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        initTabFragment()
         setTabSelectedListener()
+    }
+
+    private fun initTabFragment() {
+        supportFragmentManager.beginTransaction().apply {
+            add(R.id.fcv_tab_fragment, findPwFragment)
+            add(R.id.fcv_tab_fragment, findIdFragment) // 위로 쌓이는 구조라서 FindPwFragment 부터 add함
+        }.commit()
     }
 
     private fun setTabSelectedListener() {
         binding.tlFindIdPassword.addOnTabSelectedListener(object: TabLayout.OnTabSelectedListener {
             override fun onTabSelected(tab: TabLayout.Tab?) {
-                TODO("Not yet implemented")
+                when (tab?.position) {
+                    0 -> supportFragmentManager.beginTransaction().replace(R.id.fcv_tab_fragment, findIdFragment).commit()
+                    1 -> supportFragmentManager.beginTransaction().replace(R.id.fcv_tab_fragment, findPwFragment).commit()
+                }
             }
 
-            override fun onTabUnselected(tab: TabLayout.Tab?) {
-                TODO("Not yet implemented")
-            }
+            override fun onTabUnselected(tab: TabLayout.Tab?) { }
 
-            override fun onTabReselected(tab: TabLayout.Tab?) {
-                TODO("Not yet implemented")
-            }
+            override fun onTabReselected(tab: TabLayout.Tab?) { }
         })
     }
 
